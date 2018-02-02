2edd8054db311fd198b069e15aa1b01a5d53fe67
Alexander Suter <alexander.suter@axonivy.com>
2018-02-02 13:50:11 +0100

    Create new method, which cause a fault

--- country-service/src/main/java/ch/ivyteam/testservice/country/CamelRoute.java
+++ country-service/src/main/java/ch/ivyteam/testservice/country/CamelRoute.java
@@ -7,6 +7,7 @@ import org.apache.camel.builder.RouteBuilder;
 import ch.ivyteam.testservice.country.incident.IncidentService;
 import ch.ivyteam.testservice.country.incident.InputReportIncident;
 import ch.ivyteam.testservice.country.incident.OutputReportIncident;
+import ch.ivyteam.testservice.country.incident.OutputStatusIncident;
 
 public class CamelRoute extends RouteBuilder {
 
@@ -21,14 +22,19 @@ public class CamelRoute extends RouteBuilder {
             // the name of the operation is in that header
             .recipientList(simple("direct:${header.operationName}"));
 
+        from("direct:returnSoapFault")
+            .process(new Processor() {
+                public void process(Exchange exchange) throws Exception {
+                  throw new Exception("SOAP Fault Message - Business Message");
+                }
+            })
+            .to("log:output");
+        
         // report incident
         from("direct:reportIncident")
             .process(new Processor() {
                 public void process(Exchange exchange) throws Exception {
-                    // get the id of the input
                     String id = exchange.getIn().getBody(InputReportIncident.class).getIncidentId();
-
-                    // set reply including the id
                     OutputReportIncident output = new OutputReportIncident();
                     output.setCode("OK;" + id);
                     exchange.getOut().setBody(output);
@@ -40,11 +46,9 @@ public class CamelRoute extends RouteBuilder {
         from("direct:statusIncident")
             .process(new Processor() {
                 public void process(Exchange exchange) throws Exception {
-                    // set reply
-                    //OutputStatusIncident output = new OutputStatusIncident();
-                    //output.setStatus("IN PROGRESS");
-                    //exchange.getOut().setBody(output);
-					throw new Exception("alex faulllts");
+                    OutputStatusIncident output = new OutputStatusIncident();
+                    output.setStatus("IN PROGRESS");
+                    exchange.getOut().setBody(output);
                 }
             })
             .to("log:output");
--- country-service/src/main/java/ch/ivyteam/testservice/country/incident/IncidentService.java
+++ country-service/src/main/java/ch/ivyteam/testservice/country/incident/IncidentService.java
@@ -1,6 +1,7 @@
 package ch.ivyteam.testservice.country.incident;
 
 public interface IncidentService {
+    void returnSoapFault();
     OutputReportIncident reportIncident(InputReportIncident input);
     OutputStatusIncident statusIncident(InputStatusIncident input);
 }
