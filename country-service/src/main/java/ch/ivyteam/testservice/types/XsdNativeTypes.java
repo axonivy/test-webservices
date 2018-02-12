eb2919551d0d623b8274f7ea748cf806ef294c94
Reguel <reguel.wermelinger@ivyteam.ch>
2018-02-12 16:32:32 +0100

    introduce an xsd:dateType value

--- country-service/src/main/java/ch/ivyteam/testservice/types/XsdNativeTypes.java
+++ country-service/src/main/java/ch/ivyteam/testservice/types/XsdNativeTypes.java
@@ -3,10 +3,16 @@ package ch.ivyteam.testservice.types;
 import java.time.ZonedDateTime;
 import java.util.Date;
 
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import ch.ivyteam.testservice.types.adapter.ZoneDateTimeAdapter;
+
 public class XsdNativeTypes {
 
 	public String string;
 	public Date date;
+	
+	@XmlJavaTypeAdapter(ZoneDateTimeAdapter.class) // make xsd:dateType compatible
 	public ZonedDateTime dateTime;
 	
 }
